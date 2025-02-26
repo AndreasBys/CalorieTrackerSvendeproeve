83f5d8505bcc789feb8adfeb31c0609397bebf98
DanielHoyerHare <danielhoyerhare@gmail.com>
2025-02-26 12:29:40 +0100

    Tilføjet config fil

--- API/index.js
+++ API/index.js
@@ -4,16 +4,9 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 
 // importing route modules
-import authRoutes from './routes/authRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import foodRoutes from './routes/foodRoutes.js';
-import dailyIntakeRoutes from './routes/dailyIntakeRoutes.js';
-import goalRoutes from './routes/goalRoutes.js';
 
-// setting environment variables for secret key, mongodb url, and port
-process.env.SECRET_KEY = 'test';
-process.env.MONGODB_URL = 'mongodb://0.0.0.0:27017/appDB';
-process.env.PORT = 5000;
+// importing config
+import config from './config.js';
 
 // creating an instance of the express application
 const app = express();
@@ -26,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // function to connect to mongodb using mongoose
 const dbConnect = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL, {
+        await mongoose.connect(config.MONGODB_URL, {
             autoIndex: true,  // automatically build indexes
         });
         console.log('connected to db');  // log successful connection
@@ -40,16 +33,9 @@ const dbConnect = async () => {
 dbConnect();
 
 // start the server and listen on the specified port
-app.listen(process.env.PORT, () => {
-    console.log(`listening on port: http://localhost:${process.env.PORT}`);
+app.listen(config.PORT, () => {
+    console.log(`listening on port: http://localhost:${config.PORT}`);
 });
-
-// define routes for different functionalities
-app.use('/api/auth', authRoutes);  // authentication routes
-app.use('/api/users', userRoutes);  // user-related routes
-app.use('/api/foods', foodRoutes);  // food-related routes
-app.use('/api/dailyIntakes', dailyIntakeRoutes);  // daily intake routes
-app.use('/api/goals', goalRoutes);  // goal-related routes
 
 // define a route for the root url that sends a welcome message
 app.get('/', (req, res) => res.send('hello from homepage'));
