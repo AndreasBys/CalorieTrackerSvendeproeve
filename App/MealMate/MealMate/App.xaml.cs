3c4fa92b0c1e652aa1b64e40eb23a892f2b8cc1a
Abdey Habdey <andreaszzz51@gmail.com>
2025-02-28 16:05:52 +0100

    Registrer mål side

--- App/MealMate/MealMate/App.xaml.cs
+++ App/MealMate/MealMate/App.xaml.cs
@@ -4,6 +4,7 @@
     {
         public App()
         {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NMaF1cXmhKYVRpR2Nbek55flRFalxQVAciSV9jS3tTdEdrWXZfc3RURWBYWA==");
             InitializeComponent();
 
             MainPage = new AppShell();
--- App/MealMate/MealMate/Platforms/Android/MainApplication.cs
+++ App/MealMate/MealMate/Platforms/Android/MainApplication.cs
@@ -22,6 +22,32 @@ namespace MealMate
                     handler.PlatformView.SetHintTextColor(ColorStateList.ValueOf(Android.Graphics.Color.Red));
                 }
             });
+
+            Microsoft.Maui.Handlers.PickerHandler.Mapper.AppendToMapping(nameof(Picker), (handler, view) =>
+            {
+                if (view is Picker)
+                {
+                    // Remove underline
+                    handler.PlatformView.BackgroundTintList = ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+
+                    // Change placeholder text color
+                    handler.PlatformView.SetHintTextColor(ColorStateList.ValueOf(Android.Graphics.Color.White));
+                }
+            });
+
+            Microsoft.Maui.Handlers.DatePickerHandler.Mapper.AppendToMapping(nameof(DatePicker), (handler, view) =>
+            {
+                if (view is DatePicker)
+                {
+                    // Remove underline
+                    handler.PlatformView.BackgroundTintList = ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+
+                    // Change placeholder text color
+                    handler.PlatformView.SetHintTextColor(ColorStateList.ValueOf(Android.Graphics.Color.White));
+                }
+            });
+
+
         }
 
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
