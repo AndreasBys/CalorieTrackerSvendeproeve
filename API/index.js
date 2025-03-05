44bccf7ca94fc279096aadd7949cbf678e7161d8
DanielHoyerHare <danielhoyerhare@gmail.com>
2025-03-05 17:58:17 +0100

    index rettet

--- API/index.js
+++ API/index.js
@@ -1,19 +1,19 @@
 // importing necessary libraries
-import express          from 'express';
-import bodyParser       from 'body-parser';
-import cron             from 'node-cron';
+import express              from 'express';
+import bodyParser           from 'body-parser';
+import cron                 from 'node-cron';
 
 // importing route modules
-import authRoutes       from './routes/authRoutes.js';
-import dishRoutes       from './routes/dishRoutes.js';
-import foodRoutes       from './routes/foodRoutes.js';
-import macroGoalRoutes  from './routes/macroGoalRoutes.js';
-import macroLogRoutes   from './routes/macroLogRoutes.js';
-import userRoutes       from './routes/userRoutes.js';
+import authRoutes           from './routes/authRoutes.js';
+import dishRoutes           from './routes/dishRoutes.js';
+import foodRoutes           from './routes/foodRoutes.js';
+import macroGoalRoutes      from './routes/macroGoalRoutes.js';
+import macroLogRoutes       from './routes/macroLogRoutes.js';
+import userRoutes           from './routes/userRoutes.js';
 
 // importing files
-import config           from './config.js';
-import dbConnect        from './db.js';
+import config               from './config.js';
+import dbConnect            from './db.js';
 
 // importing saveAllMacroLogs for cron job
 import { saveAllMacroLogs } from './controllers/macroLogLegacyController.js';
@@ -31,11 +31,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // connects to database
 dbConnect();
 
+
 // start the server and listen on the specified port
 app.listen(config.PORT, () => {
     console.log(`listening on port: http://localhost:${config.PORT}`);
 });
-
 
 // define routes
 app.use('/api/auth', authRoutes);               // authenticator-related routes
@@ -44,11 +44,6 @@ app.use('/api/food', foodRoutes);               // food-related routes
 app.use('/api/macroGoal', macroGoalRoutes);     // macroGoal-related routes
 app.use('/api/macroLog', macroLogRoutes);       // macroLog-related routes
 app.use('/api/users', userRoutes);              // user-related routes
-
-
-
-// defines a route for the root url that sends a welcome message
-app.get('/', (req, res) => res.send('hello from homepage'));
 
 // Kører hver dag kl. 23:59 UTC+1
 cron.schedule("59 22 * * *", async () => {
