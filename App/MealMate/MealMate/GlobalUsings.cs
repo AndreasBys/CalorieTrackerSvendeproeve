25c8d52a1aee1c42e4351a50c088cefab497da8b
DanielHoyerHare <danielhoyerhare@gmail.com>
2025-03-10 17:53:25 +0100

    Food ændringer

--- App/MealMate/MealMate/GlobalUsings.cs
+++ App/MealMate/MealMate/GlobalUsings.cs
@@ -1,6 +1,7 @@
-﻿global using MealMate.ViewModel;
-global using System.Globalization;
-global using System.Threading.Tasks;
+﻿global using MealMate.ViewModels;
 global using CommunityToolkit.Mvvm.Input;
 global using MealMate.View;
+global using MealMate.Models;
+global using MealMate.Services;
+global using System.Diagnostics;
 
--- App/MealMate/MealMate/ViewModels/RegistrerMaalSideViewModel.cs
+++ App/MealMate/MealMate/ViewModels/RegistrerMaalSideViewModel.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MealMate.ViewModel
+namespace MealMate.ViewModels
 {
     public partial class RegistrerMaalSideViewModel : BaseViewModel
     {
