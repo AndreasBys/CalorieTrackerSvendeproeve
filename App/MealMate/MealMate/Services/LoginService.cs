df63ecc56949c0e3d3efb619fb39ec7ad4b5b50f
Bjarkebek <bjarke44@gmail.com>
2025-03-12 16:46:28 +0100

    tilføjet loginservice og Usermodel

---
+++ App/MealMate/MealMate/Services/LoginService.cs
@@ -0,0 +1,39 @@
+﻿using MealMate.Services.Interfaces;
+using System.Net.Http.Json;
+
+namespace MealMate.Services;
+
+public class LoginService : ILoginService
+{
+    private readonly HttpClient _httpClient;
+    private string _authToken;
+
+    public LoginService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<User> Login(string email, string password)
+    {
+        var loginRequest = new { email = email, password = password };
+        var response = await _httpClient.PostAsJsonAsync("/api/auth/login", loginRequest);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var loginResponse = await response.Content.ReadFromJsonAsync<LoginResponse>();
+            _authToken = loginResponse.Token;
+            return loginResponse.User;
+        }
+        else
+        {
+            var errorContent = await response.Content.ReadAsStringAsync();
+            throw new Exception($"Login failed: {response.StatusCode}, {errorContent}");
+        }
+    }
+}
+
+public class LoginResponse
+{
+    public string Token { get; set; }
+    public User User { get; set; }
+}
